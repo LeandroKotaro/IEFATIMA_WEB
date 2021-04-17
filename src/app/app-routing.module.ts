44cf629756990ff24db2c376ff9cae178caa8137
Daniel <161765@unsaac.edu.pe>
2021-04-17 08:49:48 -0500

    Enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GaleriaComponent } from './galeria/galeria.component';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { OrganizacionComponent } from './organizacion/organizacion.component';
 
 const routes: Routes = [
-  {path:'Galeria',component: GaleriaComponent}
+  {path:'',component:HomeComponent},
+  {path:'Home',component:HomeComponent},
+  {path:'Galeria',component: GaleriaComponent},
+  {path:'Nosotros',component:NosotrosComponent},
+  {path:'Organizacion',component:OrganizacionComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { GaleriaComponent } from './galeria/galeria.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { FooterColegioComponent} from './footer-colegio/footer-colegio.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterColegioComponent,
     NavbarComponent,
     GaleriaComponent,
     NosotrosComponent,
